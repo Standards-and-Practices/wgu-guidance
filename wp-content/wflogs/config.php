0a8bfc3387a287f360e5dbcb5c85391edd90fd18
David Petersen <david.petersen@wgu.edu>
2022-06-23 18:04:20 +0000

    Merged existing code from https://guidance.wgu.edu

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"oz(,Fc ^8?Uh{e;$KC9gq!;(4FlNBDbS}9gqbc:L%)[x7F02y47U~Lw;% CH080J";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:443;}
