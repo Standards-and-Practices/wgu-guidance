b21ac9b5215c39ff5e199c9afac99fc97d97bd8d
David Petersen <dp@davidp.us>
2022-07-07 15:50:59 -0600

    Add try catch to custom filters

--- functions.php
+++ functions.php
@@ -5,69 +5,83 @@
  */
 function wgu_theme_child_scripts()
 {
-    wp_enqueue_style( 'wgu-theme-stylesheet', get_stylesheet_uri(), array( 'hello-elementor' ), wp_get_theme());
-    wp_enqueue_style( 'elementor-frontend-css', plugins_url('/elementor/assets/css/frontend.min.css'), array( 'hello-elementor' ), wp_get_theme());
-} 
+  wp_enqueue_style(
+    'wgu-theme-stylesheet',
+    get_stylesheet_uri(),
+    ['hello-elementor'],
+    wp_get_theme()
+  );
+  wp_enqueue_style(
+    'elementor-frontend-css',
+    plugins_url('/elementor/assets/css/frontend.min.css'),
+    ['hello-elementor'],
+    wp_get_theme()
+  );
+}
 add_action('wp_enqueue_scripts', 'wgu_theme_child_scripts');
-
 
 /**
  *  Add category label as color
  */
 if (!function_exists('get_category_label')) {
-
-    function get_category_label($category)
-    {
-        $color = get_field("color", "category_$category->cat_ID");
-        return "<span class='category' style='color: rgba(" . implode(',', $color) . ")'>$category->name</span>";
-    }
+  function get_category_label($category)
+  {
+    $color = get_field("color", "category_$category->cat_ID");
+    return "<span class='category' style='color: rgba(" .
+      implode(',', $color) .
+      ")'>$category->name</span>";
+  }
 }
 
 /**
  *  Fix facet wp collapse styling
  */
-add_filter( 'facetwp_assets', function( $assets ) {
-    FWP()->display->json['expand'] = '<span class="facet-expand"></span>';
-    FWP()->display->json['collapse'] = '<span class="facet-collapse"></span>';
-    return $assets;
+add_filter('facetwp_assets', function ($assets) {
+  FWP()->display->json['expand'] = '<span class="facet-expand"></span>';
+  FWP()->display->json['collapse'] = '<span class="facet-collapse"></span>';
+  return $assets;
 });
 
-function get_domain_color ($data) {  
-    
-    $color = '#000000';
+function get_domain_color($data)
+{
+  $color = '#000000';
+  $domains = get_the_terms($data, 'domains');
+
+  if (isset($domains[0])) {
+    $meta = get_term_meta($domains[0]->term_id);
+    $color = $meta["color"][0];
+  }
+
+  return $color;
+}
+
+function get_domain_icon($data)
+{
+  $icon = get_stylesheet_directory_uri("images/all.svg");
+
+  try {
     $domains = get_the_terms($data, 'domains');
 
     if (isset($domains[0])) {
-        $meta = get_term_meta($domains[0]->term_id);
-        $color = $meta["color"][0];
+      $meta = get_term_meta($domains[0]->term_id);
+      $icon = wp_get_attachment_image_url($meta["active_icon"][0]);
     }
+  } catch (\Throwable $th) {
+  }
+  return $icon;
+}
 
-    return $color;
- }
-
- function get_domain_icon ($data) {  
-    
-    $icon = get_stylesheet_directory_uri("images/all.svg");
+function get_domain_inactive_icon($data)
+{
+  $icon = get_stylesheet_directory_uri("images/all.svg");
+  try {
     $domains = get_the_terms($data, 'domains');
 
     if (isset($domains[0])) {
-        $meta = get_term_meta($domains[0]->term_id);
-        $icon = wp_get_attachment_image_url($meta["active_icon"][0]);
+      $meta = get_term_meta($domains[0]->term_id);
+      $icon = wp_get_attachment_image_url($meta["inactive_icon"][0]);
     }
-
-    return $icon;
- }
-
- function get_domain_inactive_icon ($data) {  
-    
-    $icon = get_stylesheet_directory_uri("images/all.svg");
-    $domains = get_the_terms($data, 'domains');
-
-    if (isset($domains[0])) {
-        $meta = get_term_meta($domains[0]->term_id);
-        $icon = wp_get_attachment_image_url($meta["inactive_icon"][0]);
-
-    }
-
-    return $icon;
- }
+  } catch (\Throwable $th) {
+  }
+  return $icon;
+}
