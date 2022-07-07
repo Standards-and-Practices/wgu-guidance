decca35b5a9e30df06b42eb5c2feacb381585e9d
David Petersen <dp@davidp.us>
2022-07-07 15:25:59 -0600

    Add WP Query

--- page-newsearch.php
+++ page-newsearch.php
@@ -1,35 +1,17 @@
 <?php
-/**
- * The template for displaying singular post-types: posts, pages and user-defined custom post types.
- *
- * @package HelloElementor
- */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+  exit(); // Exit if accessed directly.
 }
-?>
-<?php
-while ( have_posts() ) :
-	the_post();
-	?>
 
-<main id="content" <?php post_class( 'site-main' ); ?> role="main">
-	<?php if ( apply_filters( 'hello_elementor_page_title', true ) ) : ?>
-		<header class="page-header">
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		</header>
-	<?php endif; ?>
-	<div class="page-content">
-		<?php the_content(); ?>
-		<div class="post-tags">
-			<?php the_tags( '<span class="tag-links">' . __( 'Tagged ', 'hello-elementor' ), null, '</span>' ); ?>
-		</div>
-		<?php wp_link_pages(); ?>
-	</div>
+// Get WP Header
+get_header();
 
-	<?php comments_template(); ?>
-</main>
+global $query_string;
 
-	<?php
-endwhile;
+wp_parse_str( $query_string, $search_query );
+$search = new WP_Query( $search_query );
+
+var_dump($search);
+
+get_footer();
