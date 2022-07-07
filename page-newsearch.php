70502e06a4e7368ac658f74790aa3c3ed3ec4d15
David Petersen <dp@davidp.us>
2022-07-07 15:22:18 -0600

    Use single from elementor

--- page-newsearch.php
+++ page-newsearch.php
@@ -1,18 +1,35 @@
 <?php
+/**
+ * The template for displaying singular post-types: posts, pages and user-defined custom post types.
+ *
+ * @package HelloElementor
+ */
 
-if (!defined('ABSPATH')) {
-  exit(); // Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
+?>
+<?php
+while ( have_posts() ) :
+	the_post();
+	?>
 
-get_header();
-?>
-<div class="elementor-container elementor-column-gap-wider">
-	<div class="elementor-column elementor-col-33 elementor-top-column elementor-element">
-		Sidebar
+<main id="content" <?php post_class( 'site-main' ); ?> role="main">
+	<?php if ( apply_filters( 'hello_elementor_page_title', true ) ) : ?>
+		<header class="page-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header>
+	<?php endif; ?>
+	<div class="page-content">
+		<?php the_content(); ?>
+		<div class="post-tags">
+			<?php the_tags( '<span class="tag-links">' . __( 'Tagged ', 'hello-elementor' ), null, '</span>' ); ?>
+		</div>
+		<?php wp_link_pages(); ?>
 	</div>
-	<div class="elementor-column elementor-col-66 elementor-top-column elementor-element">
-		Main content
-	<div>
-</div>
-<?php get_footer();
-?>
+
+	<?php comments_template(); ?>
+</main>
+
+	<?php
+endwhile;
